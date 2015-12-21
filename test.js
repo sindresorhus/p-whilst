0a1f09291aad2ffdb15615e3d43d034dbc160ec8
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-21 16:54:32 +0100

    remove now moot XO ignore

--- test.js
+++ test.js
@@ -5,9 +5,7 @@ test('whilst', async t => {
 	const result = [];
 	let counter = 0;
 
-	await fn(() => {
-		return result.length < 7
-	}, () => {
+	await fn(() => result.length < 7, () => {
 		result.push(counter++);
 	});
 
@@ -19,14 +17,12 @@ test('works with action returning a promise', async t => {
 	const result = [];
 	let counter = 0;
 
-	await fn(() => {
-		return result.length < 7;
-	}, () => {
-		return new Promise(resolve => {
+	await fn(() => result.length < 7, () =>
+		new Promise(resolve => {
 			result.push(counter++);
 			resolve();
-		});
-	});
+		})
+	);
 
 	t.is(counter, 7);
 	t.same(result, [0, 1, 2, 3, 4, 5, 6]);
@@ -36,23 +32,21 @@ test('stops on error', async t => {
 	const result = [];
 	let counter = 0;
 
-	const prom = fn(() => {
-		return result.length < 7;
-	}, () => {
-		return new Promise(resolve => {
+	const prom = fn(() => result.length < 7, () =>
+		new Promise(resolve => {
 			if (counter === 7) {
 				throw new Error('BAAD');
 			}
 
 			result.push(counter++);
 			resolve();
-		});
-	});
+		})
+	);
 
 	try {
 		await prom;
 	} catch (err) {
-		t.is(e.message, 'BAAD');
+		t.is(err.message, 'BAAD');
 	}
 
 	t.is(counter, 7);
